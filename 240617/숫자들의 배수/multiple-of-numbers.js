7b36e3191cf5f862fbd6106d0679487920bd7796
Sahqmo <123519675+Sahqmo@users.noreply.github.com>
2024-06-17 14:46:38 +0900

    Title: 숫자들의 배수, Time: 259ms, Memory: 14MB, Status: Passed - CodeTree

--- 240617/숫자들의 배수/multiple-of-numbers.js
+++ 240617/숫자들의 배수/multiple-of-numbers.js
@@ -3,9 +3,12 @@ let input = Number(fs.readFileSync(0))
 
 let mul5 = []
 let count = 1
+let str = ""
 
 while (mul5.length < 2) {
-    process.stdout.write(input * count + " ");
+    str += input * count + " "
     if (input * count % 5 === 0) mul5.push(input * count)
     count++;
 }
+
+console.log(str)
