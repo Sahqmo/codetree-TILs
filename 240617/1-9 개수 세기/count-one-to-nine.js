8f5d245be33ec885af9af6bbd64e641a5d3ffc23
Sahqmo <123519675+Sahqmo@users.noreply.github.com>
2024-06-17 21:49:02 +0900

    Title: 1-9 개수 세기, Time: 233ms, Memory: 14MB, Status: Passed - CodeTree

--- 240617/1-9 개수 세기/count-one-to-nine.js
+++ 240617/1-9 개수 세기/count-one-to-nine.js
@@ -6,7 +6,10 @@ let b = input[1].split(" ").map(Number);
 
 let arr = Array(9).fill(0);
 
-for (let i = 0; i < arr.length; i++) {
+for (let i = 0; i < amt; i++) {
     arr[b[i] - 1] += 1
+}
+
+for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
 }
