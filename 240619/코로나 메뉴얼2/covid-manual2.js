4fc729a3bb3c66c3f9298a21cb2af91a575e3282
Sahqmo <123519675+Sahqmo@users.noreply.github.com>
2024-06-19 00:38:54 +0900

    Title: 코로나 메뉴얼2, Time: 225ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240619/코로나 메뉴얼2/covid-manual2.js
+++ 240619/코로나 메뉴얼2/covid-manual2.js
@@ -1,12 +1,14 @@
 let fs = require("fs");
 let input = fs.readFileSync(0).toString().split("\n");
 
+
 let arr = Array(4).fill(0);
 let str = ""
 
 for (let i = 0; i < input.length; i++) {
-    let is_sick = input[i][0] === "Y" ? true : false
-    let temperature = input[i][1]
+    let person_info = input[i].split(" ")
+    let is_sick = person_info[0] === "Y" ? true : false
+    let temperature = person_info[1]
 
     if (is_sick) {
         arr[0 + 2 * Number(temperature >= 37)] += 1
