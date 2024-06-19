62191eb0e16d7c0f540a1aa64ab5dc30528c410a
Sahqmo <123519675+Sahqmo@users.noreply.github.com>
2024-06-19 10:45:34 +0900

    Title: 코로나 메뉴얼2, Time: 197ms, Memory: 47MB, Status: Passed - CodeTree

--- 240619/코로나 메뉴얼2/covid-manual2.js
+++ 240619/코로나 메뉴얼2/covid-manual2.js
@@ -1,29 +1,36 @@
-let fs = require("fs");
-let input = fs.readFileSync(0).toString().split("\n");
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
 
+let countArr = Array(5).fill(0);
+let typeNum = 0;
+let s, t;
 
-let arr = Array(4).fill(0);
-let str = ""
+// s와 t를 입력받은 후 카운팅 배열을 통해 각각의 빈도 저장
+for (let i = 0; i < 3; i++) {
+    [s, t] = input[i].split(" ");
 
-for (let i = 0; i < input.length; i++) {
-    let person_info = input[i].split(" ")
-    let is_sick = person_info[0] === "Y" ? true : false
-    let temperature = person_info[1]
-
-    if (is_sick) {
-        arr[2 - 2 * Number(temperature >= 37)] += 1
+    if (t >= 37 && s === "Y") {
+        typeNum = 1;
+    } else if (t >= 37) {
+        typeNum = 2;
+    } else if (s === "Y") {
+        typeNum = 3;
     } else {
-        arr[3 - 2 * Number(temperature >= 37)] += 1
+        typeNum = 4;
     }
+
+    countArr[typeNum]++;
 }
 
-
-for (let i = 0; i < arr.length; i++) {
-    str += arr[i] + " ";
+// A부터 D까지 나온 횟수를 출력
+let str = "";
+for (let i = 1; i <= 4; i++) {
+    str += countArr[i] + " ";
 }
 
-if (arr[0] >= 2) {
-    str += "E"
+if (countArr[1] >= 2) {
+    str += "E";
 }
 
-console.log(str)
+console.log(str);
