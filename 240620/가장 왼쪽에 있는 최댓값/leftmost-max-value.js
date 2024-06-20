66daa5d6c9036997bd7b6d913bc084a7bcd6dc6f
Sahqmo <123519675+Sahqmo@users.noreply.github.com>
2024-06-20 10:28:32 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 184ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240620/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
+++ 240620/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
@@ -2,17 +2,22 @@ let fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 
 let arr = input[1].split(" ").map(Number);
-let downfilter = arr.slice()
-downfilter.sort((a, b) => b - a);
+let maxVal;
 
 let str = ""
 
-while (arr.length != 1){
+while (arr.length > 1){
+    maxVal = Number.MIN_SAFE_INTEGER;
     for (i in arr) {
-        if (arr[i] === downfilter[0]) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i]
+        }
+    }
+
+    for (i in arr) {
+        if (arr[i] === maxVal) {
             str += (Number(i) + 1) + " ";
-            arr.length = Number(i) + 1;
-            downfilter = arr.slice()
+            arr.length = Number(i);
             break;
         }
     }
