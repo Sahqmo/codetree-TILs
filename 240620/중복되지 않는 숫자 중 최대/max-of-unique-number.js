cba2fffdd76d51f5d4bd58c87008f06423d83be7
Sahqmo <123519675+Sahqmo@users.noreply.github.com>
2024-06-20 09:59:45 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 247ms, Memory: 18MB, Status: Passed - CodeTree

--- 240620/중복되지 않는 숫자 중 최대/max-of-unique-number.js
+++ 240620/중복되지 않는 숫자 중 최대/max-of-unique-number.js
@@ -3,14 +3,22 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 
 let arr = input[1].split(" ").map(Number);
 
-let maxVal = -1
+let store = [-1]
 let two_times = []
 
 for (idx in arr) {
-    if (!two_times.includes(arr[idx]) && arr[idx] > maxVal) {
-        maxVal = arr[idx];
+    // 중복 수 배열에 없다면 (새로 등장한 숫자라면)
+    if (!two_times.includes(arr[idx])) {
         two_times.push(arr[idx])
+        store.push(arr[idx])
+    // 중복 수 배열에 있다면 (이미 등장한 숫자라면)
+    } else {
+        if (store.includes(arr[idx])) {
+            store = store.filter((e) => e !== arr[idx])
+        }
     }
 }
 
-console.log(maxVal)
+store.sort((a, b) => {return b - a})
+
+console.log(store[0])
