6f6b6f504d7b6eb490ef8611aab6005f6a7a3971
Sahqmo <123519675+Sahqmo@users.noreply.github.com>
2024-06-20 09:39:21 +0900

    Title: n개의 숫자 중 최대 2개, Time: 302ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240620/n개의 숫자 중 최대 2개/two-max-of-n-num.js
@@ -0,0 +1,9 @@
+let fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n")
+
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+
+arr.sort((a, b) => { return b - a })
+
+console.log(arr[0], arr[1])
