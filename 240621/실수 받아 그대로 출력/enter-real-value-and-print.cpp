9e59ba120b0d9082cc1ab87a8b52920be538a5fc
Sahqmo <123519675+Sahqmo@users.noreply.github.com>
2024-06-21 20:02:57 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240621/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << fixed;
+    double n;
+    cin >> n;
+
+    cout.precision(2);
+    cout << n;
+    return 0;
+}
