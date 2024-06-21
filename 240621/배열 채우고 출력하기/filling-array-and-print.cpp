1c257b26bf1f409c6ae8651118c6de7a7508ef59
Sahqmo <123519675+Sahqmo@users.noreply.github.com>
2024-06-21 20:41:48 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240621/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char a[10];
+    for (int i = 0; i < 10; i++) {
+        cin >> a[i];
+    }
+    for (int i = 10; i > 0; i--) {
+        cout << a[i-1];
+    }
+    return 0;
+}
