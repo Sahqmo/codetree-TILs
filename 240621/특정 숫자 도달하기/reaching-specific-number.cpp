56a5c8bd551fc0b4023d15231cb849530e59ea6d
Sahqmo <123519675+Sahqmo@users.noreply.github.com>
2024-06-21 20:48:30 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240621/특정 숫자 도달하기/reaching-specific-number.cpp
+++ 240621/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -5,7 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int val;
     int sum = 0;
-    double avg;
+    double avg = 0;
 
     for (int i = 0; i < 10; i++) {
         cin >> val;
@@ -15,6 +15,9 @@ int main() {
         }
         sum += val;
     }
+
+    if (avg == 0)
+        avg = (double)sum / 10;
 
     cout << fixed;
     cout.precision(1);
