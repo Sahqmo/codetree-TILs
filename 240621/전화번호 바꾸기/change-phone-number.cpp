9dafa63cfea6e505a0584fc516f5066e73b1cf40
Sahqmo <123519675+Sahqmo@users.noreply.github.com>
2024-06-21 20:17:56 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240621/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, x, y;
+    char b;
+    cin >> a >> b >> x >> b >> y;
+    cout << "010" << b << y << b << x;
+    return 0;
+}
